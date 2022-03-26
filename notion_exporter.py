f00c4ef02e9b4b437778e0ac8b41f3b5c96a630c
Ivo Bellin Salarin <nilleb@users.noreply.github.com>
2022-03-26 15:36:47 +0100

    chore: separate responsibilities

--- notion_exporter.py
+++ notion_exporter.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 import logging
 import os
@@ -10,30 +11,24 @@ from slugify import slugify
 from notion_client import NotionApiClient
 
 
-class NotionCrawler(object):
+class Crawler(object):
     EXPORT_FOLDER = "notion-export"
 
-    def __init__(
-        self, token, root_pages, export_folder=EXPORT_FOLDER, resume=False
-    ) -> None:
-        self.client = NotionApiClient(token)
+    def __init__(self, root_pages, export_folder=EXPORT_FOLDER, resume=False) -> None:
         self.export_folder = export_folder
         self._resume_buffer_and_visited(root_pages, resume)
 
         if not os.path.isdir(self.export_folder):
             os.makedirs(self.export_folder)
 
+    def compute_visited(self):
+        raise NotImplementedError("Please Implement this method")
+
     def _buffer_file_path(self):
         return f"{self.export_folder}/buffer.json"
 
     def _visited_file_path(self):
         return f"{self.export_folder}/visited.json"
-
-    def _compute_visited(self):
-        self.visited = {}
-        for filepath in glob(f"{self.export_folder}/*.json"):
-            uid = filepath[-41:-5]
-            self.visited[uid] = filepath
 
     def _resume_buffer_and_visited(self, buffer, resume):
         self.buffer = []
@@ -49,7 +44,7 @@ class NotionCrawler(object):
                 with open(self._visited_file_path()) as fd:
                     self.visited = dict(json.load(fd))
             except:
-                self._compute_visited()
+                self.compute_visited()
 
         if not self.buffer:
             self.buffer = buffer
@@ -71,8 +66,39 @@ class NotionCrawler(object):
         with open(path, "w") as fd:
             json.dump(self.visited, fd)
 
-    def _buffer_append(self, type, id, title):
+    def append_to_buffer(self, type, id, title):
         self.buffer.append({"type": type, "id": id, "title": title})
+
+    def crawl(self):
+        items = self.buffer
+
+        item = items.pop(0) if items else None
+
+        while item:
+            kind = item.get("type")
+            uid = item.get("id")
+            title = item.get("title")
+
+            if uid not in self.visited:
+                logging.info(f"{datetime.utcnow().isoformat()} crawl {kind} {uid}")
+                getattr(self, f"crawl_{kind}")(uid, title)
+                self.visited[uid] = item
+
+            self._persist_buffer_and_history()
+
+            item = self.buffer.pop(0) if self.buffer else None
+
+
+class NotionCrawler(Crawler):
+    def __init__(self, token, **kwargs) -> None:
+        self.client = NotionApiClient(token)
+        super().__init__(**kwargs)
+
+    def compute_visited(self):
+        self.visited = {}
+        for filepath in glob(f"{self.export_folder}/*.json"):
+            uid = filepath[-41:-5]
+            self.visited[uid] = filepath
 
     def dump(self, object_id, title, data):
         title = slugify(title)[:64] if title else None
@@ -114,10 +140,10 @@ class NotionCrawler(object):
         bid = block.get("id")
 
         if block_type == "child_page":
-            self._buffer_append("page", bid, self._child_title(block))
+            self.append_to_buffer("page", bid, self._child_title(block))
 
         if block_type == "child_database":
-            self._buffer_append("database", bid, self._child_title(block))
+            self.append_to_buffer("database", bid, self._child_title(block))
 
         return block
 
@@ -135,8 +161,6 @@ class NotionCrawler(object):
         return "-".join([part.get("plain_text") for part in title_parts])
 
     def crawl_page(self, page_id, title=None):
-        logging.info(f"crawling page {page_id}")
-
         page = self.client.get_page(page_id)
         if page.get("archived"):
             logging.warning(f"The page {page.get('url')} is archived. Skipping.")
@@ -149,18 +173,15 @@ class NotionCrawler(object):
         self.dump(page_id, title, page)
 
     def crawl_database_item(self, item_id, title=None):
-        logging.info(f"crawling db item {item_id}")
         blocks = self.process_single_block(item_id)
         self.dump(item_id, title, {"blocks": blocks})
 
     def crawl_database(self, database_id, title=None):
-        logging.info(f"crawling db {database_id}")
-
         database = self.client.get_database(database_id)
 
         items = list(self.client.paginate_children_items(database_id))
         for item in items:
-            self._buffer_append(
+            self.append_to_buffer(
                 "database_item",
                 item.get("id"),
                 self._object_title(**self._title_property(item)),
@@ -169,30 +190,6 @@ class NotionCrawler(object):
 
         title = title if title else self._object_title(database)
         self.dump(database_id, title, database)
-
-    def crawl(self):
-        items = self.buffer
-
-        item = items.pop(0) if items else None
-
-        while item:
-            kind = item.get("type")
-            uid = item.get("id")
-            title = item.get("title")
-
-            if uid not in self.visited:
-                if kind == "page":
-                    self.crawl_page(uid, title=title)
-                elif kind == "database":
-                    self.crawl_database(uid, title=title)
-                elif kind == "database_item":
-                    self.crawl_database_item(uid, title=title)
-
-                self.visited[uid] = item
-
-            self._persist_buffer_and_history()
-
-            item = self.buffer.pop(0) if self.buffer else None
 
 
 if __name__ == "__main__":
